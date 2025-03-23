270055fe9935a632dd2f9c625cbeead625e5342d
KngCd <panaligankingcedrick@gmail.com>
2025-03-23 11:14:23 +0800

    add new folders

---
+++ lourenzSpace/lourenz.js
@@ -0,0 +1,15 @@
+DOM.insertAdjacentHTML(
+  "beforeend",
+  `
+
+    <!-- Lourenz Space -->
+
+    <!-- Kayo na bahala kung ano ilalagay nyo dine HAHAHHH -->
+
+    <div class="vh-100 d-flex flex-column justify-content-center align-items-center text-center bg-warning"> 
+        <h1>Lourenz Section</h1>
+        <p>hello perds</p>
+    </div>
+
+`
+);
